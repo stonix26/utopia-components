661e78013023cacc415f926aa46d56861d0d60a3
Ruston Emperua <stonix26@gmail.com>
2023-10-26 07:09:35 +0800

    added new component - calendar

---
+++ apps/docs/stories/calendar.stories.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { Calendar } from '@utopia/calendar'
+
+function CalendarComponent() {
+  const [date, setDate] = useState<Date | undefined>(new Date())
+  return (
+    <Calendar
+      className="rounded-md border"
+      mode="single"
+      onSelect={setDate}
+      selected={date}
+    />
+  )
+}
+
+const meta: Meta<typeof Calendar> = {
+  component: Calendar
+}
+
+export default meta
+
+type Story = StoryObj<typeof Calendar>
+
+export const Default: Story = {
+  render: () => <CalendarComponent />
+}
