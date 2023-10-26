73def600269aca9dbd5e0120b724798e0f84296f
Ruston Emperua <stonix26@gmail.com>
2023-10-26 17:42:27 +0800

    added checkbox component

---
+++ apps/docs/stories/checkbox.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Checkbox } from '@utopia/checkbox'
+
+const meta: Meta<typeof Checkbox> = {
+  component: Checkbox
+}
+
+export default meta
+
+type Story = StoryObj<typeof Checkbox>
+
+export const Default: Story = {
+  render: props => (
+    <div className="flex items-center space-x-2">
+      <Checkbox {...props} />
+      <label
+        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        htmlFor="terms"
+      >
+        Accept terms and conditions
+      </label>
+    </div>
+  ),
+  name: 'Checkbox',
+  args: {
+    id: 'terms'
+  }
+}
