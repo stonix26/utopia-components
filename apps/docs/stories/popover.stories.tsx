dba627d66828c6031ffd2772cfae3bab3c1e13cf
Ruston Emperua <stonix26@gmail.com>
2023-11-20 19:49:04 +0800

    added popover component

---
+++ apps/docs/stories/popover.stories.tsx
@@ -0,0 +1,67 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button } from '@utopia/button'
+import { Input } from '@utopia/input'
+import { Label } from '@utopia/label'
+import { Popover, PopoverContent, PopoverTrigger } from '@utopia/popover'
+
+const meta: Meta<typeof Popover> = {
+  component: Popover
+}
+
+export default meta
+
+type Story = StoryObj<typeof Popover>
+
+export const Default: Story = {
+  render: props => (
+    <Popover {...props}>
+      <PopoverTrigger asChild>
+        <Button variant="outline">Open popover</Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-80">
+        <div className="grid gap-4">
+          <div className="space-y-2">
+            <h4 className="font-medium leading-none">Dimensions</h4>
+            <p className="text-sm text-muted-foreground">
+              Set the dimensions for the layer.
+            </p>
+          </div>
+          <div className="grid gap-2">
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="width">Width</Label>
+              <Input
+                className="col-span-2 h-8"
+                defaultValue="100%"
+                id="width"
+              />
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="maxWidth">Max. width</Label>
+              <Input
+                className="col-span-2 h-8"
+                defaultValue="300px"
+                id="maxWidth"
+              />
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="height">Height</Label>
+              <Input
+                className="col-span-2 h-8"
+                defaultValue="25px"
+                id="height"
+              />
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="maxHeight">Max. height</Label>
+              <Input
+                className="col-span-2 h-8"
+                defaultValue="none"
+                id="maxHeight"
+              />
+            </div>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  )
+}
