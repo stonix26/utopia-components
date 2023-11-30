32fe5a2592dfd3539294ce9004d652095d45c0fb
Ruston Emperua <stonix26@gmail.com>
2023-11-30 21:03:55 +0800

    added form story

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  extends: ['custom/storybook']
+  extends: ['custom/storybook'],
+  rules: {
+    '@typescript-eslint/no-misused-promises': [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false
+        }
+      }
+    ]
+  }
 }
---
+++ apps/docs/stories/form.stories.tsx
@@ -0,0 +1,76 @@
+import type { Meta } from '@storybook/react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { type SubmitHandler, useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { Button } from '@utopia/button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@utopia/form'
+import { Input } from '@utopia/input'
+import { toast } from '@utopia/toast'
+
+const meta: Meta = {
+  title: 'Form'
+}
+
+export default meta
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.'
+  })
+})
+
+type TFormSchema = z.infer<typeof formSchema>
+
+export function ProfileForm(): JSX.Element {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: ''
+    }
+  })
+
+  // 2. Define a submit handler.
+  const onSubmit: SubmitHandler<TFormSchema> = (values: TFormSchema) => {
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      )
+    })
+  }
+
+  return (
+    <Form {...form}>
+      <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="stonix26" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
