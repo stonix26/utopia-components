bf1f1c05c8cb6ac356481abc07100776256591ec
Ruston Emperua <stonix26@gmail.com>
2024-01-15 10:31:48 +0800

    added new resizable package

---
+++ apps/docs/stories/resizable.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup
+} from '@utopia/resizable'
+
+const meta: Meta<typeof ResizablePanel> = {
+  component: ResizablePanel
+}
+
+export default meta
+
+type Story = StoryObj<typeof ResizablePanelGroup>
+
+export const Default: Story = {
+  render: args => (
+    <ResizablePanelGroup {...args}>
+      <ResizablePanel defaultSize={50}>
+        <div className="flex h-[200px] items-center justify-center p-6">
+          <span className="font-semibold">One</span>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={50}>
+        <ResizablePanelGroup direction="vertical">
+          <ResizablePanel defaultSize={25}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Two</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={75}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Three</span>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  ),
+  args: {
+    className: 'max-w-md rounded-lg border',
+    direction: 'horizontal'
+  }
+}
