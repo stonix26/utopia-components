821b02f8ab9e175ee34474a44199b1231d165008
Ruston Emperua <stonix26@gmail.com>
2024-02-06 17:49:31 +0800

    fix react types versions

--- apps/email-app/src/App.tsx
+++ apps/email-app/src/App.tsx
@@ -1,8 +1,20 @@
+import { Button } from '@utopia/button'
+import { AirVentIcon } from 'lucide-react'
+import { cn } from '@utopia/classnames'
+
 function App() {
   return (
-    <div className="bg-slate-200">
-      <h1 className="text-orange-500">Hello Email</h1>
-    </div>
+    <>
+      <div className="bg-slate-200">
+        <h1 className={cn('text-orange-500', { 'bg-black': true })}>
+          Hello Email
+        </h1>
+        <Button type="button" size="lg">
+          Hello
+          <AirVentIcon className="h-4 w-4" />
+        </Button>
+      </div>
+    </>
   )
 }
 
--- apps/email-app/tailwind.config.js
+++ apps/email-app/tailwind.config.js
@@ -4,7 +4,11 @@ import TailwindTypography from '@tailwindcss/typography'
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    '../../packages/**/src/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     container: {
       center: true,
