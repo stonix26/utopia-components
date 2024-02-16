18367ccee04406c69990fb3e2054f8cc1df1ffec
Ruston Emperua <stonix26@gmail.com>
2024-02-16 22:40:57 +0800

    testing react-aria components

---
+++ apps/docs/stories/react-aria-components/button.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button } from '@utopia/rac-button'
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  args: {
+    type: 'button',
+    variant: 'default',
+    size: 'default'
+  },
+  argTypes: {
+    type: {
+      control: { type: 'radio' },
+      options: ['button', 'submit', 'reset']
+    },
+    variant: {
+      control: { type: 'radio' },
+      description: 'lorem50',
+      options: [
+        'default',
+        'destructive',
+        'outline',
+        'secondary',
+        'ghost',
+        'link'
+      ]
+    },
+    size: {
+      control: { type: 'radio' },
+      options: ['default', 'xs', 'sm', 'lg', 'icon']
+    }
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof Button>
+
+export const Default: Story = {
+  render: args => <Button {...args} />,
+  args: {
+    children: 'RAC Button',
+    onPress: () => {
+      // eslint-disable-next-line no-alert -- testing only
+      alert('Hello from Turborepo!')
+    }
+  }
+}
