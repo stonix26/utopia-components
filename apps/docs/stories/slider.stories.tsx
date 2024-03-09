87e2bd55ad94e6535c3ad6506358bab62b4f5e94
Ruston Emperua <stonix26@gmail.com>
2024-03-09 15:50:19 +0800

    fix slider implementation

--- apps/docs/stories/slider.stories.tsx
+++ apps/docs/stories/slider.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Slider } from '@utopia/radix-slider'
+import { useState } from 'react'
 
 const meta: Meta<typeof Slider> = {
   component: Slider
@@ -10,5 +11,27 @@ export default meta
 type Story = StoryObj<typeof Slider>
 
 export const Default: Story = {
-  render: () => <Slider className="w-[60%]" defaultValue={[50]} max={100} />
+  render: () => <Slider className="w-[60%]" defaultValue={[25]} />
+}
+
+const arrayToString = (array: number[]): string => {
+  return `[${array.join(', ')}]`
+}
+
+export function Controlled(): JSX.Element {
+  const [value, setValue] = useState([130, 188, 243])
+  return (
+    <div className="flex flex-col items-center gap-y-4">
+      <pre>
+        <code>value: {arrayToString(value)}</code>
+      </pre>
+      <Slider
+        className="w-full"
+        max={360}
+        min={0}
+        onValueChange={setValue}
+        value={value}
+      />
+    </div>
+  )
 }
--- radix/slider/src/slider.tsx
+++ radix/slider/src/slider.tsx
@@ -11,21 +11,33 @@ import { cn } from '@utopia/classnames'
 const Slider = forwardRef<
   ElementRef<typeof SliderPrimitive.Root>,
   ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <SliderPrimitive.Root
-    className={cn(
-      'relative flex w-full touch-none select-none items-center',
-      className
-    )}
-    ref={ref}
-    {...props}
-  >
-    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
-  </SliderPrimitive.Root>
-))
+>(({ className, value, defaultValue, ...props }, ref) => {
+  const internalValue = value ?? defaultValue
+
+  return (
+    <SliderPrimitive.Root
+      className={cn(
+        'relative flex w-full touch-none select-none items-center',
+        className
+      )}
+      defaultValue={defaultValue}
+      ref={ref}
+      value={value}
+      {...props}
+    >
+      <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
+        <SliderPrimitive.Range className="absolute h-full bg-primary" />
+      </SliderPrimitive.Track>
+      {internalValue?.map((_, i) => (
+        <SliderPrimitive.Thumb
+          className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+          // eslint-disable-next-line react/no-array-index-key -- TEMP
+          key={i}
+        />
+      ))}
+    </SliderPrimitive.Root>
+  )
+})
 Slider.displayName = SliderPrimitive.Root.displayName
 
 export { Slider }
